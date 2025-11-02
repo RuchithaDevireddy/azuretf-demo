6edd8cb13ba9620cf7176e790cbe051b55bde2a3
shiva <narrashivakumarreddy@gmail.com>
2025-11-02 17:17:44 +0000

    changes5

--- main.tf
+++ main.tf
@@ -1,26 +1,15 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=4.14.0"
-    }
+  required_version = ">= 1.3.0"
+
+  backend "azurerm" {
+    resource_group_name   = "test-demo-rg"
+    storage_account_name  = "testdemostorage963"
+    container_name        = "dev"
+    key                   = "terraform.tfstate"
+    use_azuread_auth      = true
   }
 }
-# Configure the Microsoft Azure Provider
+
 provider "azurerm" {
   features {}
-  subscription_id      = "23804002-cbd4-473c-972d-2fffa924c8aa"
-# Create a Resource Group
-resource "azurerm_resource_group" "example" {
-  name     = "rg-terraform-demoo"
-  location = "East US"
-}
-
-# Create a Storage Account
-resource "azurerm_storage_account" "example" {
-  name                     = "tfstoragedemo1234"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
 }
