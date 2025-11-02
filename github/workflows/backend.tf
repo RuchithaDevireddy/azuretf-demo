c6553c28bcc05f609958e24076fe1f3d440529a1
shiva <narrashivakumarreddy@gmail.com>
2025-11-02 14:28:31 +0000

    Initial commit

---
+++ github/workflows/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "azurerm" {
+   resource_group_name   = "test-demo-rg"
+   storage_account_name  = "testdemostorage963"
+   container_name        = "dev"
+   key                   = "terraform.tfstate"
+   use_azuread_auth      = true
+   subscription_id      = "23804002-cbd4-473c-972d-2fffa924c8aa"
+  }
+}
