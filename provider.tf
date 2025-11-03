ca779132718838f3ee011e455c9299891651a22e
RuchithaDevireddy <168339586+RuchithaDevireddy@users.noreply.github.com>
2025-11-04 00:28:35 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,5 +9,9 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  subscription_id      = "23804002-cbd4-473c-972d-2fffa924c8aa"
+  client_id       = "3005f381-57b1-4b4f-9532-83c6a5a32f4c"
+  client_secret   = var.client_secret
+  tenant_id       = "9a4b9bfc-6430-4d00-833c-a8826bf2df43"
+  subscription_id = "20000000-0000-0000-0000-000000000000"
+
 }
